faf59ce85392a6eaf04400847016ae013774a285
Richard Weber <richard.weber@nitor.com>
2020-12-03 15:51:41 +0200

    302 is OK, so check for that instead

--- src/common/utils/fetchPortals.ts
+++ src/common/utils/fetchPortals.ts
@@ -1,7 +1,13 @@
 import {Portal, PortalPayload} from '../../types'
 
 const fetchPortals = async (): Promise<Portal[]> => {
-  const payload: PortalPayload[] = await fetch(`/api/portal`).then(r => r.json())
+  const payload: PortalPayload[] = await fetch(`/api/portal`).then(r => {
+    if (r.status === 302) {
+      window.location.reload()
+      return
+    }
+    return r.json()
+  })
   return payload.map(p => ({
     connection: p.connection,
     size: p.size,
