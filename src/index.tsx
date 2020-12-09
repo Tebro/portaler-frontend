4ecf316738e42fdababb94c6b33a9b57bbcf8a66
Richard Weber <richard.weber@nitor.com>
2020-12-09 15:42:20 +0200

    Refresh app after one hour

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,10 @@ import { Provider } from 'react-redux'
 import App from './App'
 import store from './store'
 
+setTimeout(() => {
+  window.location.reload()
+}, 60 * 60 * 1000) // Reload page after an hour
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
